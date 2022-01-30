17f2516c18105409ebade518677eafac4be7a016
ArvikVan <arvikvan@gmail.com>
2022-01-30 22:52:56 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/accident/control/IndexControl.java
+++ src/main/java/accident/control/IndexControl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 /**
  * @author ArvikV
  * @version 1.0
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> stringList = List.of("one", "two", "three", "four", "five");
+        model.addAttribute("NameOfRefToJSP", stringList);
         return "index";
     }
 }
