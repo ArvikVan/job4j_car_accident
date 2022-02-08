2a31d4b3cb3915b7a699a953c9407e3eec76e716
ArvikVan <arvikvan@gmail.com>
2022-02-08 21:08:03 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/accident/SecurityInit.java
@@ -0,0 +1,11 @@
+package accident;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 08.02.2022
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
